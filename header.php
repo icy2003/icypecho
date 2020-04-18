d639fed1e13a70f80b36b84a935c5baf2334aee9
icy2003 <2317216477@qq.com>
2020-04-19 02:48:27 +0800

    ~

--- header.php
+++ header.php
@@ -22,43 +22,6 @@
     <script src="<?php $this->options->themeUrl('vendor/bower/clipboard/dist/clipboard.min.js'); ?>"></script>
     <script src="<?php $this->options->themeUrl('vendor/bower/layui/dist/layui.js'); ?>"></script>
     <script src="<?php $this->options->themeUrl('js/main.js'); ?>"></script>
-
-    <script>
-    // 鼠标漂浮字
-    $(document).click(function(e) {
-        var list = ['富强', '民主', '文明', '和谐', '自由', '平等', '公正', '法治', '爱国', '敬业', '诚信', '友善'];
-        textUp(e, 2000, list, 200);
-    })
-
-    function textUp(e, time, arr, heightUp) {
-        var lists = Math.floor(Math.random() * arr.length);
-        var colors = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
-        var $i = $('<span />').text(arr[lists]);
-        var xx = e.pageX || e.clientX + document.body.scroolLeft;
-        var yy = e.pageY || e.clientY + document.body.scrollTop;
-
-        $('body').append($i);
-        $i.css({
-            'font-family': '"Microsoft JhengHei","Apple LiGothic Medium,Microsoft YaHei","微软雅黑","Arial",sans-serif',
-            'font-size': '20px',
-            'font-weight': 'bold',
-            cursor: 'default',
-            top: yy,
-            left: xx,
-            color: colors,
-            transform: 'translate(-50%, -50%)',
-            display: 'block',
-            position: 'absolute',
-            zIndex: 999999999999
-        })
-        $i.animate({
-            top: yy - (heightUp ? heightUp : 200),
-            opacity: 0
-        }, time, function() {
-            $i.remove();
-        })
-    }
-    </script>
     <script>
     var _hmt = _hmt || [];
     (function() {
